1e17443b5e87c5db540b9edfe380861e28b0aeac
Umesh Yadav <umeshyadavrocks@gmail.com>
2022-04-02 09:26:58 +0530

    Dynamic use of navbar to menuItem page

--- scripts/menuItem.js
+++ scripts/menuItem.js
@@ -479,7 +479,18 @@ let freshlyData = [
     
     })
     }
+    let dayselect = document.getElementById('meal-menu')
+dayselect.addEventListener("click",function(){
+        let planValue = 4;
+        let planPrice = "$8.99 / meal";
 
+        localStorage.setItem("planValue",JSON.stringify(planValue));
+        localStorage.setItem("planPrice",JSON.stringify(planPrice));
+
+        window.location.assign("../day.html");
+
+    })
+    
     let navbarSignup = document.getElementById("sign-up");
     navbarSignup.addEventListener("click",function(){
         window.location.assign("./signup.html");
